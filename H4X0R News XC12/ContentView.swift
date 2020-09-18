6b50fcfe53f33f7f24fc708f6e7150ee0cf8dd5f
Kwin Sirikwin <k.sirikw@gmail.com>
2020-09-18 09:25:23 +0700

    Initial Commit

---
+++ H4X0R News XC12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News XC12
+//
+//  Created by Kwin Sirikwin on 18/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R News XC12/H4X0R_News_XC12App.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_XC12App.swift
+//  H4X0R News XC12
+//
+//  Created by Kwin Sirikwin on 18/9/20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_XC12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
